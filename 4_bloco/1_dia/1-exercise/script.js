39572b7e309b007f65414349028804cb6adab0ab
Gabriel Lenz <lenzordmusic@gmail.com>
2021-05-19 19:50:35 -0400

    Exercício 2

--- 4_bloco/1_dia/1-exercise/script.js
+++ 4_bloco/1_dia/1-exercise/script.js
@@ -1,27 +1,29 @@
+ //EXERCÍCIO 1
+
 let a = 10
 let b = 5
 
-// 1º Programa:
+ //1º Programa:
 
 let adicao = a + b
-console.log(adicao)
+    console.log(adicao)
 
 // 2º Programa:
 
 let subtracao = a - b
-console.log(subtracao)
+    console.log(subtracao)
 
 // 3º Programa
 
 let multiplicacao = a * b
-console.log(multiplicacao)
+   console.log(multiplicacao)
 
 // 4º Programa
 
 let divisao = a / b
-console.log(divisao)
+    console.log(divisao)
 
-// 5º Programa
+//5º Programa
 
 let modulo = a % b
-console.log(modulo)
+    console.log(modulo)
---
+++ 4_bloco/1_dia/2-exercise/script.js
@@ -0,0 +1,10 @@
+// EXERCÍCIO 2
+
+let a = 44;
+let b = 101;
+
+if(a > b) {
+    console.log(a)
+} else {
+    console.log(b)
+}

c9a16349382e011c21c2958da70f5dc3c99d4836
Gabriel Lenz <lenzordmusic@gmail.com>
2021-05-19 19:35:29 -0400

    Exercício 1

--- 4_bloco/1_dia/1-exercise/script.js
+++ 4_bloco/1_dia/1-exercise/script.js
@@ -1,3 +1,27 @@
-let name = 'Gabriel';
+let a = 10
+let b = 5
 
-console.log(name);
+// 1º Programa:
+
+let adicao = a + b
+console.log(adicao)
+
+// 2º Programa:
+
+let subtracao = a - b
+console.log(subtracao)
+
+// 3º Programa
+
+let multiplicacao = a * b
+console.log(multiplicacao)
+
+// 4º Programa
+
+let divisao = a / b
+console.log(divisao)
+
+// 5º Programa
+
+let modulo = a % b
+console.log(modulo)

c01491eacf1e10481209a89b74722071fa79689d
Gabriel Lenz <lenzordmusic@gmail.com>
2021-05-19 20:09:18 -0400

    Exercício 3

---
+++ 4_bloco/1_dia/script.js
@@ -0,0 +1,13 @@
+// EXERCÍCIO 3
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+if(a > b && a > c) {
+    console.log('O let (a) é maior que (b) e (c)');
+} else if(b > a && b > c) {
+    console.log('O let (b) é maior que (a) e (c)');
+} else {
+    console.log('O let (c) é maior que (a) e (c)');
+}

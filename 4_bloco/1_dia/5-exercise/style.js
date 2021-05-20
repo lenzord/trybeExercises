ea9dd446716dba5d2cfc4a08716260c0534cef69
Gabriel Lenz <lenzordmusic@gmail.com>
2021-05-19 21:18:40 -0400

    Exercício 6

--- 4_bloco/1_dia/5-exercise/style.js
+++ 4_bloco/1_dia/5-exercise/style.js
@@ -1,14 +1,16 @@
 // EXERCÍCIO 5
 
-let a = -60;
-let b = -60;
+let a = 70;
+let b = 60;
 let c = 60;
 let somaABC = a + b + c;
 
-if (somaABC < 0) {
-    console.log("Erro fatal") 
-} else if(somaABC > 0 && somaABC == 180) {
-    console.log(true);
-} else if(somaABC > 0 && somaABC != 180) {
-    console.log(false);
+if(a > 0 && b > 0 && c > 0) {
+    if(somaABC == 180) {
+        console.log(true);
+} else {
+        console.log(false);
+} 
+    } else {
+    console.log("Erro fatal");
 }

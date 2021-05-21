57219814358529e31d1dabf45a4136dbb66aaf0e
Gabriel Lenz <lenzordmusic@gmail.com>
2021-05-20 22:32:28 -0400

    Exercicio 2

---
+++ 4_bloco/2_dia/2-exercicio/style.js
@@ -0,0 +1,7 @@
+let numeros = [10, 52, 9, 12, 13, 70, 19, 24, 63, 99];
+let resultado = 0;
+
+for(let index = 0; index < numeros.length; index += 1) {
+    resultado += numeros[index];
+    console.log(resultado);
+}

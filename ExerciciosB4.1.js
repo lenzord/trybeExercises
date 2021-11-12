7f394b8b815e7109b5f8b727877451209a6b0b88
Gabriel <lenzordmusic@gmail.com>
2021-11-12 15:17:14 -0400

    Exercicios ate o 4

--- ExerciciosB4.1.js
+++ ExerciciosB4.1.js
@@ -3,7 +3,7 @@
 // 1)Faça cinco programas, um para cada operação aritmética básica.
 // Seu programa deve ter duas constantes, a e b , definidas no começo 
 // com os valores que serão operados.
- 
+
 // 1.1) Adição
 
 const Sum = (a, b) => {
@@ -33,3 +33,37 @@ const Division = (a, b) => {
 const Module = (a, b) => {
   return a % b;
 }
+
+
+// 2) Faça um programa que retorne o maior de dois números. 
+// Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const TernaryBiggestNumber = (a, b) => {
+  return a > b ? a : b;
+}
+
+// 3) Faça um programa que retorne o maior de três números. 
+// Defina no começo do programa três constantes com os valores que serão comparados.
+
+const IfBiggestNumber = (a, b, c) => {
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else if (c > a && c > b) {
+    return c;
+  }
+}
+
+// 4) Faça um programa que, dado um valor definido numa constante,
+// retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+const isValue = (value) => {
+  if(value > 0) {
+    return  'O valor é positivo'
+  } else if (value < 0) {
+    return 'O valor é negativo'
+  } else return 'O valor é zero'
+}
+
+// 5) 

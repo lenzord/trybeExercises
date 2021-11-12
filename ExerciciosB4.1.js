8ce14d1b259fd935f4a6f7dd35eefbaf03873c0a
Gabriel <lenzordmusic@gmail.com>
2021-11-12 14:37:49 -0400

    Dia 4.1

---
+++ ExerciciosB4.1.js
@@ -0,0 +1,35 @@
+// ============================ DIA 4.1 - PRIMEIROS PASSOS =========================================== //
+
+// 1)Faça cinco programas, um para cada operação aritmética básica.
+// Seu programa deve ter duas constantes, a e b , definidas no começo 
+// com os valores que serão operados.
+ 
+// 1.1) Adição
+
+const Sum = (a, b) => {
+  return a + b;
+}
+
+// 1.2) Subtração
+
+const Subtraction = (a, b) => {
+  return a - b;
+}
+
+// 1.3) Multiplicação 
+
+const Multiplication = (a, b) => {
+  return a * b;
+}
+
+// 1.4) Divisão
+
+const Division = (a, b) => {
+  return a * b;
+}
+
+// 1.5) Módulo 
+
+const Module = (a, b) => {
+  return a % b;
+}
